7a8f3892cf4e3bebaaacf02ca51665b9bc88779d
mzluckylady6969 <117005118+mzluckylady6969@users.noreply.github.com>
2024-05-26 19:55:29 -0600

    Update getsign.sh

--- 02BreakECDSAcryptography/getsign.sh
+++ 02BreakECDSAcryptography/getsign.sh
@@ -4,7 +4,7 @@ for run in {1..4}; do
     export LINE=1 ; sed -n "${LINE}p" index.json > index2.json
     sed -i '1d' index.json
     echo "with open('index2.json') as myfile:" >> fileopen.py
-    echo "    listfile='\n'.join(f'python2 breakECDSA.py {line.rstrip()[:]} >> signatures.json' for line in myfile)" >> fileopen.py
+    echo "    listfile='\n'.join(f'python3 breakECDSA.py {line.rstrip()[:]} >> signatures.json' for line in myfile)" >> fileopen.py
     echo "f = open('signscript.sh', 'a')" >> fileopen.py
     echo "f.write('#!/bin/bash' + '\n')" >> fileopen.py
     echo "f.write('' + listfile + '' + '\n')" >> fileopen.py
